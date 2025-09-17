7c6761b20e0135f8231190b68be34fe3a813c4c6
sofvdvel <vandevelde.sofie@telenet.be>
2025-09-17 11:13:35 +0200

    remove space between title and img

--- modules/home_static.R
+++ modules/home_static.R
@@ -3,40 +3,25 @@ homeTabUI <- function(id) {
   ns <- NS(id)
   tabPanel(
     "Home",
-      h2(HTML("<b>Welcome to DecoNFlow Benchmarking</b>"), style = "text-align: center;"),
+    h2(
+      HTML("<b>Welcome to DecoNFlow Benchmarking</b>"),
+      class = "welcome-title",
+      style = "text-align: center;"),
 
-      br(),br(),
-      div(h3("RRBS"), style = "text-align: center;"),  
-
-      fluidRow(
-        column(
-          width = 12,
-          align = "center",
-          tags$img(
-            src = "rank_static_homepage_RRBS_2025-06-27.svg", #"rank_static_homepage_2025-05-09.svg", 
-            style = "max-width: 50%; height: auto; display: block; margin-left: auto; margin-right: auto;"
+    fluidRow(
+      class = "funkyheatmap",
+      column(
+        width = 12,
+        align = "center",
+        tags$img(
+          src = "funkyheatmap_cropped.png",
+          style = "max-width: 90%; height: auto; display: block; margin-left: auto; margin-right: auto;"
           )
         )
       ),
-      
-      br(), br(),
-      div(h3("WGBS"), style = "text-align: center;"),  
-      
-      fluidRow(
-        column(
-          width = 12,
-          align = "center",
-          tags$img(
-            src = "rank_static_homepage_WGBS_2025-06-27.svg", #"rank_static_homepage_2025-05-09.svg", 
-            style = "max-width: 50%; height: auto; display: block; margin-left: auto; margin-right: auto;"
-          )
-        )
-      ),
-      
-      
-      br(), br(), br(), h3("Citation")
+  
+    br(), br(), br(), h3("Citation")
 
-    
   )
 }
 
