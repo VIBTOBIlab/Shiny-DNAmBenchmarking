a772e4135cf718d450d22c0e0e0418b000a8997e
sofvdvel <vandevelde.sofie@telenet.be>
2025-11-12 16:10:24 +0100

    add newest version funkyheatmap

--- modules/home_static.R
+++ modules/home_static.R
@@ -7,21 +7,38 @@ homeTabUI <- function(id) {
       HTML("<b>Welcome to DecoNFlow Benchmarking</b>"),
       class = "welcome-title",
       style = "text-align: center;"),
-
+    
     fluidRow(
       class = "funkyheatmap",
       column(
         width = 12,
         align = "center",
         tags$img(
-          src = "funkyheatmap_cropped.png",
-          style = "max-width: 90%; height: auto; display: block; margin-left: auto; margin-right: auto;"
+          src = "funky.png",
+          style = "max-width: 80%; height: auto; display: block; margin-left: auto; margin-right: auto;"
           )
         )
       ),
-  
-    br(), br(), br(), h3("Citation")
+    br(), #h3("Citation"),
 
+    # tags$blockquote(class = "paper-cite",
+    #                 tags$p(
+    #                   class = "title",
+    #                   "XXX"
+    #                 ),
+    #                 tags$p(
+    #                   class = "authors",
+    #                   paste(
+    #                     "XXX"
+    #                   )
+    #                 ),
+    #                 tags$p(
+    #                   class = "doi",
+    #                   "doi: ",
+    #                   tags$a(href = "XXX", target = "_blank", rel = "noopener", "XXXX")
+    #                 )
+    # )
+    
   )
 }
 
@@ -31,8 +48,5 @@ homeTabUI <- function(id) {
 homeTabServer <- function(id) {
   moduleServer(id, function(input, output, session) {
     # Add server-side logic for Home tab if needed (currently none)
-    
-
-    
-  })
+})
 }
