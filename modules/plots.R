b612dc50b94d4cc99f44869577a71e94a50d8005
sofvdvel <vandevelde.sofie@telenet.be>
2025-11-12 16:11:40 +0100

    update source module names

--- modules/plots.R
+++ modules/plots.R
@@ -4,19 +4,17 @@
 # ==============================================================================
 
 # Source modules
-source("modules/cfrrbs_insilico.R")
-source("modules/cfrrbs_invitro.R")
-source("modules/rrbs.R")
-source("modules/wgbs.R")
+source("modules/wgbs_tt.R")
+source("modules/rrbs_tt.R")
+source("modules/rrbs_cl.R")
 
 plotsTabUI <- function(id) {
   ns <- NS(id)
   navbarMenu(
     "Plots",
-    cfrrbsinsilicoTabUI(ns("cfRRBS_in_silico"), label = "cfRRBS in-silico"),
-    cfrrbsinvitroTabUI(ns("cfRRBS_in_vitro"), label = "cfRRBS in-vitro"),
-    rrbsTabUI(ns("RRBS"), label = "RRBS"),
-    wgbsTabUI(ns("WGBS"), label = "WGBS")
+    wgbsttTabUI(ns("WGBS-TT"), label = "WGBS-TT"),
+    rrbsttTabUI(ns("RRBS-TT"), label = "RRBS-TT"),
+    rrbsclTabUI(ns("RRBS-CL"), label = "RRBS-CL")
   ) #Close navbarMenu
   
 } # Close metricsTabUI
@@ -33,12 +31,10 @@ plotsTabServer <- function(id) {
     # Code moved to global.R
     
     # Extra modules
-    cfrrbsinsilicoTabServer("cfRRBS_in_silico")
-    cfrrbsinvitroTabServer("cfRRBS_in_vitro")
-    rrbsTabServer("RRBS")
-    wgbsTabServer("WGBS")
-      
-       
+    wgbsttTabServer("WGBS-TT")
+    rrbsttTabServer("RRBS-TT")
+    rrbsclTabServer("RRBS-CL")
+
   }) # Close moduleServer
 } # Close metricsTabServer    
     
